5c6c390e4abb0dbd2a9ef72ad7fb2a051883b85d
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-02-24 17:07:41 +0100

    Update config.py

--- api/config/config.py
+++ api/config/config.py
@@ -9,14 +9,13 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
 
 class DevConfig(Config):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     # SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')   # to run on heroku
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')   # to run locally
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')   # to run locally
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3') or os.environ.get('DATABASE_URL')   # to run locally
 
 class TestConfig(Config):
