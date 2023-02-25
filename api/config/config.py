8c779f3e277fa79d8427d533f1c12b0d82e28858
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-02-25 17:30:38 +0100

    Changed Config and added postgresql path

--- api/config/config.py
+++ api/config/config.py
@@ -4,20 +4,26 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+db_name = 'pizza_api_db'
+
+default_uri = "postgres://{}:{}@{}/{}".format('postgres', 'password', 'localhost:5432', db_name)
+
+uri = os.getenv('DATABASE_URL', default_uri) # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
+
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_SECRET_KEY = config('JWT_SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')   # to run on heroku
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')   # to run locally
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3') or os.environ.get('DATABASE_URL')   # to run locally
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -25,7 +31,9 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', False, cast=bool)
 
 config_dict = {
     'dev': DevConfig,
---
+++ setup.sh
@@ -0,0 +1,1 @@
+export DATABASE_URL = 'postgres://wvxutpmkruirtg:c8182f28f1776482dc94c323a9bf3010216276ba8a84b8e4677ef213ed51e5fc@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/deknbhj4fv9rek'
