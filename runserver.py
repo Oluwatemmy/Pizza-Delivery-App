5bec3f8d54971879b4422786d17d2e04f85c13eb
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-02-27 15:02:19 +0100

    Updated the file to run on Heroku successfully

--- runserver.py
+++ runserver.py
@@ -1,7 +1,8 @@
 from api import create_app
 from api.config.config import config_dict
 
-app = create_app(config=config_dict['prod'])
+app = create_app(config=config_dict['prod'])    # to run on heroku for the production stage
+# app = create_app()      # to run locally for the testing process
 
 if __name__=="__main__":
     app.run()
